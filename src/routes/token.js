2e7ce869d08062e70eefbef216c7770bcc3cddd4
JuanZTO <jcobo@zerotoonegroup.com>
2025-03-07 08:45:23 -0500

    [CHANGE] Cancel Subscription Token Validation Route

--- src/routes/token.js
+++ src/routes/token.js
@@ -35,18 +35,69 @@ router.post('/subscription/validate', handleError(TokenSchema), async (req, res)
     try {
         let shopOrigin = req.get('origin');
         let shopDomain = SHOPS_ORIGIN[shopOrigin !== 'null' ? shopOrigin : 'https://hotshapers.com'];
-        const { shopAlias } = shopDomain;
-
-        const { email, token } = req.body;
+        const { shop, shopAlias, productFakeVariantId } = shopDomain;
+        
+        const { email, token, subscription } = req.body;
         const objectToken = await dbRepository.validateToken(shopAlias, email, token);
-
+        
         if (!objectToken) throw new Error('Email or Token Not Found');
         if (isExpired(objectToken.expire_at)) {
             await dbRepository.deleteToken(shopAlias, email);
             throw new Error('Email or Token Not Found');
         }
         if (objectToken.token !== token) throw new Error('Email or Token Not Found');
-        res.json({ message: 'Token confirmed' });
+        
+        const subscriptionImp = new SubscriptionImp(shop, shopAlias);
+        const shopifyImp = new ShopifyImp(shop, shopAlias);
+        
+        const subscriptionData = await subscriptionImp.getSubscription(email, subscription);
+        if (!subscriptionData) throw new Error('It is not possible to cancel the subscription');
+        if (subscriptionData.cyclesCompleted > 1) throw new Error('The subscription have more than 1 cycle completed');
+
+        // Si el estado en la dirección de la orden es de CALIFORNIA, cancelar normal
+        if (subscriptionData.ShippingAddress.province.toUpperCase() === 'CALIFORNIA') {
+            const subscriptionCancelled = await subscriptionImp.cancelSubscription(subscription);
+            if (!subscriptionCancelled) throw new Error('It is not possible to cancel the subscription');
+            await dbRepository.deleteToken(shopAlias, email);
+            res.json({ message: 'Subscription successfully cancelled' })
+            return;
+        }
+
+        // Si el estado en la dirección de la orden es diferente de CALIFORNIA, crear draft order.
+        // Primero se debe verificar si ya existe una draft order. Si existe, enviar invoice.
+        const draftOrderExists = await getActiveDraftOrder(shopAlias, subscription);
+        if (draftOrderExists) {
+            await shopifyImp.sendDraftOrderInvoice(draftOrderExists.draft_order);
+            res.json({ message: 'The invoice of the order was resent to continue with the cancellation of the subscription' })
+        } else {
+            const productSubscription = subscriptionData['SubscriptionLines'][0]['ProductVariant'];
+            const variantId = productSubscription['platformId'].split('/').pop();
+            const subscriptionId = await shopifyImp.productIdByVariant(variantId);
+            const productOneTime = await shopifyImp.oneTimeBySubscription(subscriptionId);
+            const quantity = Math.floor(productOneTime.price - productSubscription.price);
+            const draftOrderInput = {
+                acceptAutomaticDiscounts: false,
+                allowDiscountCodesInCheckout: false,
+                email,
+                shippingAddress: {
+                    address1: subscriptionData.ShippingAddress.address1,
+                    city: subscriptionData.ShippingAddress.city,
+                    province: subscriptionData.ShippingAddress.province,
+                    country: subscriptionData.ShippingAddress.country,
+                    zip: subscriptionData.ShippingAddress.zip,
+                },
+                lineItems: [
+                    {
+                        variantId: productFakeVariantId, // ID de la variante de 1 dólar
+                        quantity
+                    }
+                ]
+            };
+            const draftOrderId = await shopifyImp.createDraftOrder(draftOrderInput);
+            await dbRepository.saveDraftOrder(shopAlias, draftOrderId, subscription);
+            await shopifyImp.sendDraftOrderInvoice(draftOrderId)
+            res.json({ message: 'To finalize the subscription cancellation process please pay the draft order that has been created' })
+        }
     } catch (err) {
         console.log(err);
         logger.error(err.message);
