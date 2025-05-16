8b3d836208766362d523843abac72e6c620919b5
Andrey Vasquez <william20026@gmail.com>
2025-05-16 12:11:47 -0500

    [REMOVE] Collection param and set end date to discount

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -7,16 +7,18 @@ import { authenticateToken } from "../middlewares/authenticate-token.js"
 import SubscriptionImp from "../implements/skio.imp.js"
 import DBRepository from "../repositories/postgres.repository.js"
 import path from "node:path"
-import { getExpiredDraftOrders, setDraftOrderStatus, deleteDraftOrder } from "../services/draft-orders.js";
-import MessageImp from '../implements/slack.imp.js'
+import {
+  getExpiredDraftOrders,
+  setDraftOrderStatus,
+  deleteDraftOrder,
+} from "../services/draft-orders.js"
+import MessageImp from "../implements/slack.imp.js"
 import ShopifyImp from "../implements/shopify.imp.js"
 import KlaviyoImp from "../implements/klaviyo.imp.js"
-
 
 const router = Router()
 const dbRepository = new DBRepository()
 const messageImp = new MessageImp()
-
 
 router.use(
   bearerToken({
@@ -54,15 +56,16 @@ router.use(
  *          description: Returns JSON message
  */
 router.post("/draft-order-paid", authenticateToken, async (req, res) => {
-  let shopAlias, draftOrderId = '';
+  let shopAlias,
+    draftOrderId = ""
   try {
-    ({ shopAlias, draftOrderId } = req.body);
+    ;({ shopAlias, draftOrderId } = req.body)
     const {
       [`SHOP_NAME_${shopAlias}`]: shopName,
       [`SHOP_COLOR_${shopAlias}`]: shopColor,
       [`CONTACT_PAGE_${shopAlias}`]: contactPage,
-      [`EMAIL_SENDER_${shopAlias}`]: emailSender
-    } = app;
+      [`EMAIL_SENDER_${shopAlias}`]: emailSender,
+    } = app
     const mailer = new Mailer(shopAlias)
     const subscriptionImp = new SubscriptionImp(shopAlias)
     const draftOrder = `gid://shopify/DraftOrder/${draftOrderId}`
@@ -71,8 +74,8 @@ router.post("/draft-order-paid", authenticateToken, async (req, res) => {
       draftOrder
     )
     if (!draftOrderData) {
-      res.status(404).json({ message: 'Draft order not found' })
-      return;
+      res.status(404).json({ message: "Draft order not found" })
+      return
     }
 
     const subscription = await subscriptionImp.getSubscriptionInfo(
@@ -82,7 +85,7 @@ router.post("/draft-order-paid", authenticateToken, async (req, res) => {
       draftOrderData.cancel_session_id,
       draftOrderData.subscription
     )
-    if (!subscriptionCanceled) throw new Error('Subscription not cancelled')
+    if (!subscriptionCanceled) throw new Error("Subscription not cancelled")
 
     await dbRepository.deleteDraftOrder(shopAlias, draftOrder)
     await mailer.sendEmail(
@@ -99,11 +102,11 @@ router.post("/draft-order-paid", authenticateToken, async (req, res) => {
       },
       [
         {
-          filename: 'subscription_canceled.png',
+          filename: "subscription_canceled.png",
           path:
             path.resolve() +
             `/public/imgs/${shopAlias}/subscription_canceled.png`,
-          cid: 'top_banner',
+          cid: "top_banner",
         },
       ]
     )
@@ -115,17 +118,19 @@ router.post("/draft-order-paid", authenticateToken, async (req, res) => {
     logger.error(err.message)
     res.status(200).send({ message: err.message })
 
-    const errorMessage = err.message.replace(/[^\w\s]/gi, '').replace(/[\n\t]/g, ' ');
-    const errorShop = `🏪 SHOP: ${shopAlias}\\n`;
-    const errorData = `ℹ️ DRAFT ORDER ID: ${draftOrderId}\\n`;
+    const errorMessage = err.message
+      .replace(/[^\w\s]/gi, "")
+      .replace(/[\n\t]/g, " ")
+    const errorShop = `🏪 SHOP: ${shopAlias}\\n`
+    const errorData = `ℹ️ DRAFT ORDER ID: ${draftOrderId}\\n`
     const errorDescription = `📝 DESCRIPTION: ${errorMessage}\\n`
-    const errorRoute = `📌 ROUTE: /webhook/draft-order-paid`;
-    const errorFullMessage = `${errorShop}${errorData}${errorDescription}${errorRoute}`;
-    const errorTitle = '🔴 ❌ ERROR: Error while trying to delete the subscription in the webhook';
-    messageImp.toCancelSubscriptionErrors(errorFullMessage, errorTitle);
+    const errorRoute = `📌 ROUTE: /webhook/draft-order-paid`
+    const errorFullMessage = `${errorShop}${errorData}${errorDescription}${errorRoute}`
+    const errorTitle =
+      "🔴 ❌ ERROR: Error while trying to delete the subscription in the webhook"
+    messageImp.toCancelSubscriptionErrors(errorFullMessage, errorTitle)
   }
 })
-
 
 router.post("/attentive-custom-event", authenticateToken, async (req, res) => {
   try {
@@ -179,7 +184,6 @@ router.post("/attentive-custom-event", authenticateToken, async (req, res) => {
   }
 })
 
-
 /**
  *  @openapi
  *  /webhook/draft-orders-expired-delete:
@@ -203,52 +207,76 @@ router.post("/attentive-custom-event", authenticateToken, async (req, res) => {
  *        200:
  *          description: Returns JSON message
  */
-router.post("/draft-orders-expired-delete", authenticateToken, async (req, res) => {
-  let draftOrderErrors = [];
-  let shopAlias = '';
-  try {
-    shopAlias = req.body.shopAlias;
-    const expiredDraftOrders = await getExpiredDraftOrders(shopAlias);
-    for (const draftOrder of expiredDraftOrders) {
+router.post(
+  "/draft-orders-expired-delete",
+  authenticateToken,
+  async (req, res) => {
+    let draftOrderErrors = []
+    let shopAlias = ""
+    try {
+      shopAlias = req.body.shopAlias
+      const expiredDraftOrders = await getExpiredDraftOrders(shopAlias)
+      for (const draftOrder of expiredDraftOrders) {
         try {
-            await setDraftOrderStatus(draftOrder, 'PROCESSING');
-            const [message, draftOrderId] = await deleteDraftOrder(draftOrder.shop_alias, draftOrder.draft_order);
-            if (!message) {
-                const success_message = `Draft order ${draftOrderId} deleted successfully from DB and from Shopify`;
-                logger.info(success_message);
-                await setDraftOrderStatus(draftOrder, 'COMPLETED', success_message, draftOrder.retries+1);
-            } else throw Error(message);
+          await setDraftOrderStatus(draftOrder, "PROCESSING")
+          const [message, draftOrderId] = await deleteDraftOrder(
+            draftOrder.shop_alias,
+            draftOrder.draft_order
+          )
+          if (!message) {
+            const success_message = `Draft order ${draftOrderId} deleted successfully from DB and from Shopify`
+            logger.info(success_message)
+            await setDraftOrderStatus(
+              draftOrder,
+              "COMPLETED",
+              success_message,
+              draftOrder.retries + 1
+            )
+          } else throw Error(message)
         } catch (err) {
-            draftOrderErrors.push([draftOrder.draftOrder, err.message])
-            await setDraftOrderStatus(draftOrder, 'ERROR', err.message, draftOrder.retries+1);
-            logger.error(err.message);
+          draftOrderErrors.push([draftOrder.draftOrder, err.message])
+          await setDraftOrderStatus(
+            draftOrder,
+            "ERROR",
+            err.message,
+            draftOrder.retries + 1
+          )
+          logger.error(err.message)
         }
+      }
+
+      if (draftOrderErrors.length)
+        throw new Error(
+          "Error while trying to delete some expired draft orders from the webhook"
+        )
+      res.json({
+        message: `Draft orders from ${shopAlias} deleted successfully`,
+      })
+    } catch (err) {
+      console.log(err)
+      logger.error(err.message)
+      res.status(200).send({ message: err.message })
+
+      const errorMessage = err.message
+        .replace(/[^\w\s]/gi, "")
+        .replace(/[\n\t]/g, " ")
+      const errorRoute = "📌 ROUTE: /webhook/draft-orders-expired-delete"
+      const errorShop = `🏪 SHOP: ${shopAlias}\\n`
+
+      let errorData = "ℹ️ DRAFT ORDERS ERRRORS:"
+      for (const [draftOrder, message] of draftOrderErrors) {
+        errorData += `\\n📃 DO ${draftOrder}: ${message}`
+      }
+      errorData += "\\n"
+
+      const errorTitle =
+        "🔴 ❌ ERROR: Error while trying to delete some expired draft orders from the webhook"
+      const errorDescription = `📝 DESCRIPTION: ${errorMessage}\\n`
+      const errorFullMessage = `${errorShop}${errorData}${errorDescription}${errorRoute}`
+      messageImp.toCancelSubscriptionErrors(errorFullMessage, errorTitle)
     }
-
-    if (draftOrderErrors.length) throw new Error('Error while trying to delete some expired draft orders from the webhook')
-    res.json({ message: `Draft orders from ${shopAlias} deleted successfully` })
-  } catch (err) {
-    console.log(err)
-    logger.error(err.message)
-    res.status(200).send({ message: err.message })
-
-    const errorMessage = err.message.replace(/[^\w\s]/gi, '').replace(/[\n\t]/g, ' ');
-    const errorRoute = '📌 ROUTE: /webhook/draft-orders-expired-delete';
-    const errorShop = `🏪 SHOP: ${shopAlias}\\n`;
-
-    let errorData = 'ℹ️ DRAFT ORDERS ERRRORS:'
-    for (const [draftOrder, message] of draftOrderErrors) {
-      errorData += `\\n📃 DO ${draftOrder}: ${message}`;
-    }
-    errorData += '\\n';
-
-    const errorTitle = '🔴 ❌ ERROR: Error while trying to delete some expired draft orders from the webhook';
-    const errorDescription = `📝 DESCRIPTION: ${errorMessage}\\n`;
-    const errorFullMessage = `${errorShop}${errorData}${errorDescription}${errorRoute}`;
-    messageImp.toCancelSubscriptionErrors(errorFullMessage, errorTitle);
   }
-})
-
+)
 
 /**
  *  @openapi
@@ -282,20 +310,19 @@ router.post("/draft-orders-expired-delete", authenticateToken, async (req, res) 
  *          description: Returns JSON message
  */
 router.post("/create-cross-discount", authenticateToken, async (req, res) => {
-  
   try {
-    const { title, code, percentage, collection, email, shopAlias } = req.body;
+    const { title, code, percentage, endDate, email, shopAlias } = req.body
 
-    const shopifyImp = new ShopifyImp(shopAlias);
+    const shopifyImp = new ShopifyImp(shopAlias)
 
-    let today = new Date();
-    today.setHours(0,0,0,0);
-    
+    let today = new Date()
+    today.setHours(0, 0, 0, 0)
+
     const inputMutation = `{
       title: "${title}",
       code: "${code}",
       startsAt: "${today.toISOString()}",
-      endsAt: null,
+      endsAt: "${endDate}T00:00:00.000Z",
       combinesWith: {
         productDiscounts: false,
         orderDiscounts: false,
@@ -316,9 +343,9 @@ router.post("/create-cross-discount", authenticateToken, async (req, res) => {
           percentage: ${percentage}
         },
         items: {
-          all: false,
+          all: true,
           collections: {
-            add: ["gid://shopify/Collection/${collection}"],
+            add: [],
             remove: []
           },
           products: {
@@ -334,24 +361,25 @@ router.post("/create-cross-discount", authenticateToken, async (req, res) => {
       recurringCycleLimit: 1
     }`
 
-    logger.info(`INPUT MUTATION: ${inputMutation}`);
-    const codeDiscountNode = await shopifyImp.createDiscountCode(inputMutation);
+    logger.info(`INPUT MUTATION: ${inputMutation}`)
+    const codeDiscountNode = await shopifyImp.createDiscountCode(inputMutation)
 
     // Si ocurre un error al crear el código en Shopify
     if (!codeDiscountNode) {
       res.status(200).json({ message: "Error" })
-      return;
+      return
     }
 
-    const klaviyo = new KlaviyoImp(shopAlias);
-    const response = klaviyo.sendEvent('Cross discount', email, { discountCode: code });
-    res.json({ message: 'Cross discount sent successfully to Klaviyo' });
+    const klaviyo = new KlaviyoImp(shopAlias)
+    const response = klaviyo.sendEvent("Cross discount", email, {
+      discountCode: code,
+    })
+    res.json({ message: "Cross discount sent successfully to Klaviyo" })
   } catch (err) {
     console.log(err)
     logger.error(err.message)
     res.status(200).send({ message: err.message })
   }
 })
-
 
 export default router
