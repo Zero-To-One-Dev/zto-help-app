8f99607c5c6ec174a70d0d41f44b4986b45491d8
Andrey Vasquez <william20026@gmail.com>
2025-05-22 16:01:07 -0500

    [ADD] Google implement

---
+++ src/implements/google.imp.js
@@ -0,0 +1,40 @@
+import { google } from "googleapis"
+
+class GoogleImp {
+  async init() {
+    const client = await google.auth.getClient({
+      credentials: {
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+      },
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    })
+    const sheets = await google.sheets({ version: "v4", client })
+
+    return { client, sheets }
+  }
+  async appendValues(spreadsheetId, range, values) {
+    const { sheets } = await this.init()
+    await sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      includeValuesInResponse: false,
+      responseDateTimeRenderOption: "FORMATTED_STRING",
+      responseValueRenderOption: "FORMATTED_VALUE",
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values,
+      },
+    })
+  }
+  async getValues(spreadsheetId, range) {
+    const { sheets } = await this.init()
+    const res = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    })
+    return res.data.values
+  }
+}
+
+export default GoogleImp
--- src/implements/slack.imp.js
+++ src/implements/slack.imp.js
@@ -1,40 +1,44 @@
-import { CANCEL_SUBSCRIPTION_NOTIFY_CHANNELS, UPDATE_ADDRESS_NOTIFY_CHANNELS } from "../app.js";
+import {
+  CANCEL_SUBSCRIPTION_NOTIFY_CHANNELS,
+  UPDATE_ADDRESS_NOTIFY_CHANNELS,
+} from "../app.js"
 
 class SlackImp {
-    init() { }
+  init() {}
 
-    async sendMessage(channel, message, title) {
-        try {
-            await fetch("https://n8n-zto.herokuapp.com/webhook/slack-notifier-dev", {
-                method: "POST",
-                body: JSON.stringify({ channel, message, title }),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            console.log(`✅ Error notification sent 🚀`);
-        } catch (err) {
-            console.log(`❌ Error sending error notification: ${err}`);
-        }
+  async sendMessage(channel, message, title) {
+    try {
+      await fetch("https://n8n-zto.herokuapp.com/webhook/slack-notifier-dev", {
+        method: "POST",
+        body: JSON.stringify({ channel, message, title }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      console.log(`✅ Error notification sent 🚀`)
+    } catch (err) {
+      console.log(`❌ Error sending error notification: ${err}`)
     }
+  }
 
-    async toCancelSubscriptionErrors(message, title) {
-        const cancelSubscriptionChannels = CANCEL_SUBSCRIPTION_NOTIFY_CHANNELS.split(',');
-        for (const channel of cancelSubscriptionChannels) {
-            try {
-                await this.sendMessage(channel, message, title)
-            } catch (err) {}
-        }
+  async toCancelSubscriptionErrors(message, title) {
+    const cancelSubscriptionChannels =
+      CANCEL_SUBSCRIPTION_NOTIFY_CHANNELS.split(",")
+    for (const channel of cancelSubscriptionChannels) {
+      try {
+        await this.sendMessage(channel, message, title)
+      } catch (err) {}
     }
+  }
 
-    async toUpdateAddressErrors(message, title) {
-        const updateAddressChannels = UPDATE_ADDRESS_NOTIFY_CHANNELS.split(',');
-        for (const channel of updateAddressChannels) {
-            try {
-                this.sendMessage(channel, message, title)
-            } catch (err) {}
-        }
+  async toUpdateAddressErrors(message, title) {
+    const updateAddressChannels = UPDATE_ADDRESS_NOTIFY_CHANNELS.split(",")
+    for (const channel of updateAddressChannels) {
+      try {
+        this.sendMessage(channel, message, title)
+      } catch (err) {}
     }
+  }
 }
 
-export default SlackImp;
+export default SlackImp
