1cad637bd719b24c04ee959bf7bb8f2fa60d0380
Andrey Vasquez <william20026@gmail.com>
2025-08-06 10:10:06 -0500

    [CHANGE] unit

--- src/implements/skio.imp.js
+++ src/implements/skio.imp.js
@@ -150,7 +150,7 @@ class SkioImp {
     ).applyDiscountCode
   }
 
-  async pauseSubscription(subscriptionId, unit = "day", value = 15) {
+  async pauseSubscription(subscriptionId, unit = "month", value = 1) {
     const client = this.init()
     return (
       await client.request(gql`
